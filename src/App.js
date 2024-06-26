8526e96e5ad805b77973586cef43533208d2643d
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-06-26 11:46:11 +0100

    fix app width

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import AboutPage from "./pages/About";
-import CoursePage from './pages/Courses';
-import EventPage from './pages/Event';
-import GalleryPage from './pages/Gallery';
-import ResearchPage from './pages/Research';
-import ResoursePage from './pages/Resources';
-import StaffPage from './pages/Staff';
-import MainNavigation from './components/layout/MainNavigation';
-import Footer from './components/layout/Footer';
+import CoursePage from "./pages/Courses";
+import EventPage from "./pages/Event";
+import GalleryPage from "./pages/Gallery";
+import ResearchPage from "./pages/Research";
+import ResoursePage from "./pages/Resources";
+import StaffPage from "./pages/Staff";
+import MainNavigation from "./components/layout/MainNavigation";
+import Footer from "./components/layout/Footer";
 
 function App() {
-  return <div className='w-full max-w-6xl mx-auto'>
-    <MainNavigation />
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/about' element={<AboutPage />} />
-      <Route path='/courses' element={<CoursePage />} />
-      <Route path='/news-and-events' element={<EventPage />} />
-      <Route path='/gallery' element={<GalleryPage />} />
-      <Route path='/research' element={<ResearchPage />} />
-      <Route path='/resourses' element={<ResoursePage />} />
-      <Route path='/staffs' element={<StaffPage />} />
-    </Routes>
-    <Footer />
-  </div>;
+  return (
+    <div className="w-[90%] max-w-screen-2xl mx-auto">
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/courses" element={<CoursePage />} />
+        <Route path="/news-and-events" element={<EventPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/research" element={<ResearchPage />} />
+        <Route path="/resourses" element={<ResoursePage />} />
+        <Route path="/staffs" element={<StaffPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
