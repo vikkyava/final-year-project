c99715aa99469aa22782467780f64255ccf9969b
Vikkyava <adewoyevictoradedeji@gmail.com>
2024-07-03 11:43:55 +0100

    updated code

--- src/App.js
+++ src/App.js
@@ -8,23 +8,28 @@ import GalleryPage from "./pages/Gallery";
 import ResearchPage from "./pages/Research";
 import ResoursePage from "./pages/Resources";
 import StaffPage from "./pages/Staff";
-import MainNavigation from "./components/layout/MainNavigation";
+import MainHeader from "./components/layout/MainHeader";
+import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
+
 
 function App() {
   return (
-    <div className="w-[90%] max-w-screen-2xl mx-auto">
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/courses" element={<CoursePage />} />
-        <Route path="/news-and-events" element={<EventPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/research" element={<ResearchPage />} />
-        <Route path="/resourses" element={<ResoursePage />} />
-        <Route path="/staffs" element={<StaffPage />} />
-      </Routes>
+    <div>
+      <MainHeader />
+      <NavBar />
+      <div className="w-[90%] max-w-screen-2xl mx-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/courses" element={<CoursePage />} />
+          <Route path="/news-and-events" element={<EventPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/research" element={<ResearchPage />} />
+          <Route path="/resourses" element={<ResoursePage />} />
+          <Route path="/staffs" element={<StaffPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
