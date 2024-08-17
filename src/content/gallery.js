627ac1df3847bf32d761afe989118e97f0bea498
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-08-17 23:09:57 +0100

    gallery

---
+++ src/content/gallery.js
@@ -0,0 +1,63 @@
+export const gallery = [
+    {
+        img: '/assets/gallery_1.jpg',
+        title: 'Image 1',
+        desc: 'This is the first image in the gallery'
+    },
+    {
+        img: '/assets/gallery_2.jpg',
+        title: 'Image 2',
+        desc: 'This is the second image in the gallery'
+    },
+    {
+        img: '/assets/gallery_3.jpg',
+        title: 'Image 3',
+        desc: 'This is the third image in the gallery'
+    },
+    {
+        img: '/assets/gallery_4.jpg',
+        title: 'Image 4',
+        desc: 'This is the fourth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_5.jpg',
+        title: 'Image 5',
+        desc: 'This is the fifth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_6.jpg',
+        title: 'Image 6',
+        desc: 'This is the sixth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_7.jpg',
+        title: 'Image 7',
+        desc: 'This is the seventh image in the gallery'
+    },
+    {
+        img: '/assets/gallery_8.jpg',
+        title: 'Image 8',
+        desc: 'This is the eighth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_9.jpg',
+        title: 'Image 9',
+        desc: 'This is the ninth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_10.jpg',
+        title: 'Image 10',
+        desc: 'This is the tenth image in the gallery'
+    },
+    {
+        img: '/assets/gallery_11.jpg',
+        title: 'Image 11',
+        desc: 'This is the eleventh image in the gallery'
+    },
+    {
+        img: '/assets/gallery_12.jpg',
+        title: 'Image 12',
+        desc: 'This is the twelfth image in the gallery'
+    }
+
+]
