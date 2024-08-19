585fdd7d850889ec9a69ddd25a0c2dc493353f9d
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-08-19 13:24:43 +0100

    new events

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,15 +1,24 @@
-import EventCard from "../components/EventCard";
+import news from "../content/news";
 
 
 function EventPage() {
-    return <div className=" mx-auto">
-        <EventCard
-        imageSrc="https://via.placeholder.com/300"
-        title="Sample Event"
-        date="June 24, 2024"
-        location="New York, NY"
-      />
-    </div>;
+    return (<div>
+      <h1 className="text-4xl font-bold mt-5">Recent News</h1>
+      <div className="flex gap-10 items-center justify-center flex-wrap py-12">
+        {news.map((newsItem) => (
+            <div key={newsItem.id} className="flex flex-col md:flex-row gap-6 items-center">
+              <div className="w-full md:w-96 h-60">
+                <img src={newsItem.imageSrc} alt={newsItem.title} className="object-cover w-full h-full rounded-lg" />
+              </div>
+              <div className="flex flex-col flex-1 gap-2">
+                <h1 className="text-2xl font-bold underline">{newsItem.title}</h1>
+                <p className="text-gray-600">{newsItem.date}</p>
+                <p className="text-gray-800">{newsItem.content}</p>
+              </div>
+            </div>
+        ))}
+    </div>
+    </div>);
 }
 
 export default EventPage;
