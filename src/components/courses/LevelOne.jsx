82e4f83a37254715d98b75e8bb70dc3c79ef259c
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-08-19 14:07:53 +0100

    add course curriculums

---
+++ src/components/courses/LevelOne.jsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(code, title, unit, status) {
+  return { code, title, unit, status };
+}
+
+const harmarttanSemesterCourses = [
+  createData('CHM 131', 'General Physical Chemistry', '3', 'R'),
+  createData('CHM 151', 'Practical Chemistry I', '1', 'R'),
+  createData('MTH 101', 'Elementary Mathematic (Math I)', '3', 'R'),
+  createData('MTH 103', 'Elementary Diff an Integral Cal.', '3', 'R'),
+  createData('STA 101', 'Introduction to Probability', '2', 'R'),
+];
+
+const rainSemesterCourses = [
+  createData('CHM 131', 'General Physical Chemistry', '3', 'R'),
+  createData('CHM 151', 'Practical Chemistry I', '1', 'R'),
+  createData('MTH 101', 'Elementary Mathematic (Math I)', '3', 'R'),
+  createData('MTH 103', 'Elementary Diff an Integral Cal.', '3', 'R'),
+  createData('STA 101', 'Introduction to Probability', '2', 'R'),
+];
+
+export default function LevelOne() {
+  return (
+   <div className='border-t pt-5'>
+    <h1 className='text-lg font-bold mb-5'>100 Level Harmattan Semester Courses</h1>
+     <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align='center'>S/N</StyledTableCell>
+            <StyledTableCell align="center">Course Code</StyledTableCell>
+            <StyledTableCell align="center">Course Title</StyledTableCell>
+            <StyledTableCell align="center">Course Unit</StyledTableCell>
+            <StyledTableCell align="center">Course Status</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {harmarttanSemesterCourses.map((course, index) => (
+            <StyledTableRow key={course.title}>
+              <StyledTableCell component="th" scope="row" align='center'>
+                {index}
+              </StyledTableCell>
+              <StyledTableCell align="center">{course.code}</StyledTableCell>
+              <StyledTableCell align="center">{course.title}</StyledTableCell>
+              <StyledTableCell align="center">{course.unit}</StyledTableCell>
+              <StyledTableCell align="center">{course.status}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    <h1 className='text-lg font-bold my-5'>100 Level Rain Semester Courses</h1>
+     <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align='center'>S/N</StyledTableCell>
+            <StyledTableCell align="center">Course Code</StyledTableCell>
+            <StyledTableCell align="center">Course Title</StyledTableCell>
+            <StyledTableCell align="center">Course Unit</StyledTableCell>
+            <StyledTableCell align="center">Course Status</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rainSemesterCourses.map((course, index) => (
+            <StyledTableRow key={course.title}>
+              <StyledTableCell component="th" scope="row" align='center'>
+                {index}
+              </StyledTableCell>
+              <StyledTableCell align="center">{course.code}</StyledTableCell>
+              <StyledTableCell align="center">{course.title}</StyledTableCell>
+              <StyledTableCell align="center">{course.unit}</StyledTableCell>
+              <StyledTableCell align="center">{course.status}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+   </div>
+  );
+}
--- src/pages/Academics.jsx
+++ src/pages/Academics.jsx
@@ -1,19 +1,107 @@
-import CourseCard from "../components/CourseInfoCard";
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CourseCard from '../components/CourseInfoCard';
+import LevelOne from '../components/courses/LevelOne';
+import LevelTwo from '../components/courses/LevelTwo';
+import LevelThree from '../components/courses/LevelThree';
+import LevelFour from '../components/courses/LevelFour';
+import LevelFive from '../components/courses/LevelFive';
 
+export default function CoursePage() {
+  return (
+    <div className='my-12'>
+        <h1 className="text-center text-3xl">Course Options</h1>
+      <div className="flex flex-wrap gap-5 py-20">
+        <CourseCard
+          imageSrc="/assets/communications.jpg"
+          // date="June 13, 2024"
+          title="Communications Engineering"
+          description="Communication Engineering in the ECE (Electrical and Computer Engineering) Department at Kwara State University focuses on the study and application of communication technologies. The course covers fundamental concepts of analog and digital communication, signal processing, wireless communication, and network systems. Students learn about the design, analysis, and implementation of communication systems used in various industries, including telecommunications, broadcasting, and data networks.
 
-function CoursePage() {
-    return <div className=" mx-auto">
-        <CourseCard 
-            imageSrc="https://via.placeholder.com/150"
-            date="June 13, 2024"
-            title="Card Title"
-            description="This is a description of the card. It provides some brief information about the content of the card
-            hfhfhhfhfhfhfhfjffjhjdndhfhfjfuii
-            riejejdutyhffncbvhfdjdjkksieufyfh
-            fbnbcbvnvjdjdhfuryhdhdjfhfueueiejd
-            hdhdjhfjfj."
+          The curriculum is designed to equip students with both theoretical knowledge and practical skills, preparing them for careers in communication engineering, telecommunications, and related fields. Students also explore emerging trends in communication technology, such as 5G networks, IoT, and satellite communication."
         />
-    </div>;
-}
+        <CourseCard
+          imageSrc="/assets/control.jpg"
+          // date="June 13, 2024"
+          title="Control Engineering"
+          description="Control Engineering in the ECE (Electrical and Computer Engineering) Department at Kwara State University focuses on the analysis and design of control systems that govern the behavior of dynamic systems. The course covers key concepts such as system modeling, feedback control, stability analysis, and the implementation of control strategies for various engineering applications.
 
-export default CoursePage;
+          Students gain a deep understanding of how to design and optimize control systems for different industries, including automotive, aerospace, manufacturing, and robotics. The curriculum includes both theoretical foundations and hands-on experience with tools and technologies used in modern control engineering. Graduates are well-prepared for careers in automation, robotics, and systems engineering, with a strong ability to develop solutions that ensure the stability and efficiency of complex systems."
+        />
+        <CourseCard
+          imageSrc="/assets/power.jpg"
+          // date="June 13, 2024"
+          title="Power system and Electrical Machine"
+          description="Power System and Electrical Machine in the ECE (Electrical and Computer Engineering) Department at Kwara State University focuses on the principles and applications of power generation, transmission, distribution, and the operation of electrical machines. The course covers key topics such as power system analysis, electrical machine theory, power electronics, and the integration of renewable energy sources into the grid.
+
+          Students learn about the design, operation, and maintenance of power systems and electrical machines, including transformers, generators, and motors. The curriculum emphasizes both theoretical knowledge and practical skills, preparing students to tackle challenges in the energy sector. Graduates are equipped to work in industries such as power generation, transmission, distribution, and manufacturing, contributing to the development and management of efficient and sustainable electrical systems"
+        />
+      </div>
+
+        <h1 className='text-center text-3xl mb-10'>Departmental Course Outlines and Curricula</h1>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          100 LEVEL
+        </AccordionSummary>
+        <AccordionDetails>
+         <LevelOne />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          200 LEVEL
+        </AccordionSummary>
+        <AccordionDetails>
+          <LevelTwo />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          300 LEVEL
+        </AccordionSummary>
+        <AccordionDetails>
+          <LevelThree />
+        </AccordionDetails>
+      </Accordion>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel4-content"
+            id="panel4-header"
+            >
+            400 LEVEL
+            </AccordionSummary>
+            <AccordionDetails>
+              <LevelFour />
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel5-content"
+            id="panel5-header"
+            >
+            500 LEVEL
+            </AccordionSummary>
+            <AccordionDetails>
+              <LevelFive />
+            </AccordionDetails>
+        </Accordion>
+    </div>
+  );
+}
